b316af133721745b8c8856fec6c6bcedf4b08121
NguyenHao <anhhaonguyen.st@gmail.com>
2023-10-22 10:49:05 +0700

    - setup project

---
+++ MutilModule/MutilModule/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MutilModule
+//
+//  Created by NguyenHao on 22/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+}
+
